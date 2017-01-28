1ac4932059edc476494da40e52ff4ea4cc9b28a7
Tihonya <mrtihonya@gmail.com>
2017-01-28 17:31:28 +0200

    The early clumsy steps in front-end.... :/ :/

--- ResumeBlog.Web/App_Start/BundleConfig.cs
+++ ResumeBlog.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ResumeBlog.Web
 {
@@ -25,7 +24,7 @@ namespace ResumeBlog.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
